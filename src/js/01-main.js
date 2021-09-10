041e958d077b49f2dc9f03e6e14b52287b67026d
Soraya Alonso <salonsocuesta@gmail.com>
2021-09-10 14:08:15 +0200

    added the view of shows

--- src/js/01-main.js
+++ src/js/01-main.js
@@ -1,29 +1,46 @@
 'use strict';
 
 // VARIABLES
+//<-- texto q se ejecuta al cargar la página -->
 //Llamada al boton
 const button = document.querySelector('.js_btn');
 //Llamada a lo q escribe el usuario
 const userInput = document.querySelector('.js_input');
+//Array para guardar los datos que me devuelve la api
+
+const series = document.querySelector('.js_series');
+
+let seriesData = [];
+
+function paintSeries() {
+let html = '';
+
+for (const serie of seriesData) {
+    html += `<li>`;
+    html +=`<img src="${serie.show.image.medium}" alt="" />`  ;
+    html+=`<h2>${serie.show.name}</h2>` ;
+    html+= `</li>`;
+    series.innerHTML= html;
+}
+}
+
+// función para tarer datos
+function apiInfo() {
+
 //Guardar lo q escribe el usuario
 const userOption = userInput.value;
-//Array para guardar los datos que me devuelve la api
-let showData = [];
-
-// llamada a la api
-function apiInfo(){
 fetch(`//api.tvmaze.com/search/shows?q=${userOption}`)
+    //llamar al servidor
     .then(response => response.json())
-    .then((data) => { showData = data; });
-    showPrint(showData);
+    //data es lo q devuelve json
+    .then((data) => {
+    seriesData = data;
+    console.log(seriesData);
+    paintSeries();
+    });
 }
-console.log(showData);
+
+button.addEventListener('click', apiInfo);
 
 
-// function showPrint(){
-//     for (const showlist of showData) {
-        
-// }
-
-
-button.addEventListener('click', apiInfo );
+document.querySelector('.js_form').addEventListener('submit', (ev) => {ev.preventDefault();});
