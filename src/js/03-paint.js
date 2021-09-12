79d29d886e50a65813d5ef890efef7ef421a1d96
Soraya Alonso <salonsocuesta@gmail.com>
2021-09-12 23:58:17 +0200

    Added some styles

--- src/js/03-paint.js
+++ src/js/03-paint.js
@@ -4,13 +4,13 @@ function paintSeries() {
     let html = '';
   
     for (const serie of seriesData) {
-      html += `<li class="js_serie" id="${serie.show.id}">`;
+      html += `<li class="js_serie main__shows--series__li" id="${serie.show.id}">`;
       if (serie.show.image === null) {
         html += `<img src="${imageNull}" alt="" />`;
       } else {
         html += `<img src="${serie.show.image.medium}" alt="" >`;
       }
-      html += `<h2>${serie.show.name}</h2>`;
+      html += `<h3>${serie.show.name}</h3>`;
       html += `</li>`;
     }
     series.innerHTML = html;
--- src/js/04-favorites.js
+++ src/js/04-favorites.js
@@ -5,24 +5,23 @@
 
 function handleseries(ev){
   console.log(ev.currentTarget.id);
-  const selectedSerie = ev.currentTarget;
-  const selserieid = parseInt(selectedSerie.id);
+  const selectedSerie = parseInt(ev.currentTarget.id);
   const clickedSerie = seriesData.find((serie) => {
 
-  return serie.show.id === selserieid ;
+  return serie.show.id === selectedSerie ;
   });
   console.log(selectedSerie);
   console.log(clickedSerie);
   console.log(favorites);
   const favoritesFound = favorites.findIndex((fav) => {
-    return fav.show.id === selserieid;
+    return fav.show.id === selectedSerie;
   });
   if (favoritesFound === -1) {
-    favorites.push(clickedSerie); console.log(favorites);
+    favorites.push(clickedSerie);
   } else {
-    favorites.splice(favoritesFound, 1); console.log(favorites);
+    favorites.splice(favoritesFound, 1);
   }
-
+  paintFavorites();
 }
 
 
@@ -32,7 +31,7 @@ function listenshows(){
   const listshows = document.querySelectorAll('.js_serie');
   for (const serieElement of listshows) {
     serieElement.addEventListener('click', handleseries);
-    paintFavorites();
+    
   }
 }
 
@@ -43,13 +42,13 @@ series.addEventListener('click',listenshows);
 function paintFavorites() {
   let html = '';
   for (const favorite of favorites) {
-    html += `<li class="js_favorite" id="${favorite.show.id}">`;
+    html += `<li class="js_favorite main__shows--favorites__li" id="${favorite.show.id}">`;
     if (favorite.show.image === null) {
       html += `<img src="${imageNull}" alt="" />`;
     } else {
       html += `<img src="${favorite.show.image.medium}" alt="" >`;
     }
-    html += `<h2>${favorite.show.name}</h2>`;
+    html += `<h3>${favorite.show.name}</h3>`;
     html += `</li>`;
     favoriteshtml.innerHTML = html;
   }
